79f9a04df806325007434c5f8691600dc0952af9
Raveen Reddy <raveenreddy89@gmail.com>
2022-11-10 14:28:58 -0500

    refactor: archive more old auth code

---
+++ src/archive/tokenExchange.ts
@@ -0,0 +1,48 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import { getTokenResponse } from '@/helpers';
+
+export type StravaAuth = {
+  token_type: string;
+  expires_at: number;
+  expires_in: number;
+  refresh_token: string;
+  access_token: string;
+  athlete: StravaAthlete;
+};
+
+export type StravaAthlete = {
+  id: number;
+  firstname: string;
+  lastname: string;
+  bio: string;
+  city: string;
+  state: string;
+  country: string;
+  sex: string;
+  created_at: string;
+  updated_at: string;
+  weight: string;
+  profile_medium: string;
+  profile: string;
+};
+
+export default async function stravaAuth(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const query = req.query;
+  const { code } = query;
+
+  let response: StravaAuth | undefined = undefined;
+
+  if (typeof code == 'string') {
+    response = await getTokenResponse(code);
+  }
+
+  return response
+    ? res.status(200).json(response)
+    : res
+        .status(400)
+        .json({ message: `The following code: ${code} resulted in an err0r` });
+}
