8c68b647ac08285550ca2d6efbc9899429e1deb6
Raveen Reddy <raveenreddy89@gmail.com>
2022-11-10 21:30:16 -0500

    feat: fetch activities from API and display in list

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,20 +1,76 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+
+import { signIn, signOut, useSession } from 'next-auth/react';
+import { useState } from 'react';
+
 import Button from '@/components/buttons/Button';
 
-const redirectUrl = 'http://localhost:3000/';
-const scope = 'activity:read_all';
+import { StravaActivity } from '@/api/types';
 
 export default function Home() {
-  const handleLogin = () => {
-    window.location.href = `http://www.strava.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_STRAVA_CLIENT_ID}&response_type=code&redirect_uri=${redirectUrl}/exchange_token&approval_prompt=force&scope=${scope}`;
+  const { data: session, status } = useSession();
+
+  const [activities, setActivities] = useState([] as StravaActivity[]);
+
+  let imageLink = '';
+
+  if (typeof session?.user?.image == 'string') {
+    imageLink = session.user?.image;
+  }
+
+  const handleStravaRequest = async () => {
+    const response = await fetch('/api/strava/activities', {
+      method: 'POST',
+    }).then((res) => res.json());
+
+    const activityData: StravaActivity[] = response['data'][0];
+
+    setActivities(activityData);
   };
 
   return (
     <main className='flex h-screen justify-center bg-slate-300'>
       <div className='m-auto space-y-4'>
         <h1 className='text-4xl'>Connect to Strava</h1>
-        <Button className='z-30' variant='dark' onClick={handleLogin}>
+        <Button
+          className='z-30'
+          variant='dark'
+          onClick={() => signIn('strava')}
+        >
           Login
         </Button>
+        <Button className='z-30' variant='dark' onClick={() => signOut()}>
+          Logout
+        </Button>
+        <p>status: {status}</p>
+        {session && <p>{session.user?.name}</p>}
+        {session && <p>{session.user?.email}</p>}
+        {session && <img src={imageLink} />}
+        <Button onClick={handleStravaRequest} variant='light'>
+          Fetch Activities
+        </Button>
+
+        <div className='grid grid-cols-4 divide-x-2 divide-slate-700 border-2 border-slate-700 text-center'>
+          <p>Date</p>
+          <p>Name</p>
+          <p>Distance</p>
+          <p>ID</p>
+        </div>
+        {activities &&
+          activities.map((activity) => {
+            return (
+              <div
+                key={activity.id}
+                className='grid grid-cols-4 divide-x-2 divide-slate-700 border-2 border-slate-700 text-center'
+              >
+                <p>{activity.start_date}</p>
+                <p>{activity.name}</p>
+                <p>{(activity.distance * 0.000621371).toFixed(2)} miles</p>
+                <p>{activity.id}</p>
+              </div>
+            );
+          })}
       </div>
     </main>
   );
