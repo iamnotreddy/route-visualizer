997f833acae72ab9d325b33b5d1fa09797e6da79
Raveen Reddy <raveenreddy89@gmail.com>
2023-01-05 20:28:07 -0500

    feat: remove 2nd chart for now

--- src/components/VisXLineChart.tsx
+++ src/components/VisXLineChart.tsx
@@ -1,8 +1,5 @@
-import { Group } from '@visx/group';
 import {
   AnimatedAreaSeries,
-  AnimatedAxis,
-  AnimatedLineSeries,
   Annotation,
   AnnotationCircleSubject,
   AnnotationConnector,
@@ -38,35 +35,15 @@ export default function VisXLineChart({
 }: ChartProps) {
   // Define the dimensions and margins of the chart
   const width = 1000;
-  const height = 300;
 
   const accessors = {
     xAccessor: (d: DataPoint) => (d ? d.x : 0),
     yAccessor: (d: DataPoint) => (d ? d.y : 0),
   };
 
-  const [lineSeries, setLineSeries] = useState<DataPoint[]>();
   const [areaSeries, setAreaSeries] = useState<DataPoint[]>();
 
-  const [lineSeriesMetric, setLineSeriesMetric] = useState('heartRate');
   const [areaSeriesMetric, setAreaSeriesMetric] = useState('pace');
-
-  useEffect(() => {
-    const paceArray = generatePace(
-      transformArray(metrics.time),
-      transformArray(metrics.distance)
-    );
-
-    if (lineSeriesMetric == 'heartRate') {
-      setLineSeries(transformArray(metrics.heartRate));
-    } else if (lineSeriesMetric == 'distance') {
-      setLineSeries(transformArray(metrics.distance));
-    } else if (lineSeriesMetric == 'pace') {
-      setLineSeries(paceArray);
-    } else if (lineSeriesMetric == 'grade') {
-      setLineSeries(transformArray(metrics.grade_smooth));
-    }
-  }, [lineSeriesMetric, metrics]);
 
   useEffect(() => {
     const paceArray = generatePace(
@@ -87,21 +64,21 @@ export default function VisXLineChart({
 
   return (
     <div className='flex flex-row items-center space-x-4'>
-      <ChooseMetricBar setCurrentMetric={setLineSeriesMetric} />
-      {lineSeries && areaSeries && (
+      <ChooseMetricBar setCurrentMetric={setAreaSeriesMetric} />
+      {areaSeries && (
         <XYChart
           captureEvents={true}
           width={width}
-          height={height}
+          height={300}
           xScale={{
             type: 'linear',
-            domain: [0, lineSeries.length - 1],
+            domain: [0, areaSeries.length - 1],
             zero: false,
             nice: true,
           }}
           yScale={{
             type: 'linear',
-            domain: calculateDomain(lineSeries),
+            domain: calculateDomain(areaSeries),
             zero: false,
             nice: true,
           }}
@@ -109,69 +86,36 @@ export default function VisXLineChart({
             setCurrentFrame(e.datum.x)
           }
         >
-          <Group top={0} left={0}>
-            <AnimatedLineSeries
-              dataKey='line'
-              data={lineSeries}
-              {...accessors}
+          <AnimatedAreaSeries
+            dataKey='area'
+            data={areaSeries}
+            {...accessors}
+            fillOpacity={0.4}
+          />
+          <Annotation
+            dataKey='area'
+            datum={areaSeries[currentFrame]}
+            {...accessors}
+            dx={50}
+            dy={-50}
+          >
+            <AnnotationLabel
+              title={areaSeriesMetric}
+              subtitle={
+                areaSeriesMetric == 'pace'
+                  ? convertPaceValueForDisplay(areaSeries[currentFrame].y)
+                  : areaSeries[currentFrame].y.toString()
+              }
+              subtitleFontWeight={2}
+              showAnchorLine={false}
+              backgroundFill='rgba(0,150,150,0.1)'
+              backgroundPadding={10}
             />
-            <Annotation
-              dataKey='line'
-              datum={lineSeries[currentFrame]}
-              {...accessors}
-              dx={10}
-              dy={50}
-            >
-              <AnnotationLabel
-                title={lineSeriesMetric}
-                subtitle={
-                  lineSeriesMetric == 'pace'
-                    ? convertPaceValueForDisplay(lineSeries[currentFrame].y)
-                    : lineSeries[currentFrame].y.toString()
-                }
-                subtitleFontWeight={2}
-                showAnchorLine={false}
-                backgroundFill='rgba(0,150,150,0.1)'
-                backgroundPadding={10}
-              />
-              <AnnotationCircleSubject radius={4} stroke='green' />
-              <AnnotationConnector />
-            </Annotation>
-            <AnimatedAxis tickFormat={(v) => `${v}`} orientation='left' />
-          </Group>
-          <Group top={0} left={0}>
-            <AnimatedAreaSeries
-              dataKey='area'
-              data={areaSeries}
-              {...accessors}
-              fillOpacity={0.4}
-            />
-            <Annotation
-              dataKey='area'
-              datum={areaSeries[currentFrame]}
-              {...accessors}
-              dx={50}
-              dy={20}
-            >
-              <AnnotationLabel
-                title={areaSeriesMetric}
-                subtitle={
-                  lineSeriesMetric == 'pace'
-                    ? convertPaceValueForDisplay(lineSeries[currentFrame].y)
-                    : lineSeries[currentFrame].y.toString()
-                }
-                subtitleFontWeight={2}
-                showAnchorLine={false}
-                backgroundFill='rgba(0,150,150,0.1)'
-                backgroundPadding={10}
-              />
-              <AnnotationCircleSubject radius={4} stroke='green' />
-              <AnnotationConnector />
-            </Annotation>
-          </Group>
+            <AnnotationCircleSubject radius={4} stroke='green' />
+            <AnnotationConnector />
+          </Annotation>
         </XYChart>
       )}
-      <ChooseMetricBar setCurrentMetric={setAreaSeriesMetric} />
     </div>
   );
 }
