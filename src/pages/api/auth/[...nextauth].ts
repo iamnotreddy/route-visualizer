5dcb60d66afd39cf877b04d1205630ad8d41ec40
Raveen Reddy <raveenreddy89@gmail.com>
2023-04-15 15:19:34 -0400

    chore: config

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,12 +4,12 @@ import StravaProvider from 'next-auth/providers/strava';
 const {
   NEXT_PUBLIC_STRAVA_CLIENT_ID = '',
   NEXT_PUBLIC_STRAVA_CLIENT_SECRET = '',
-  // NEXT_PUBLIC_VERCEL_URL = 'https://localhost:3000',
-  // NODE_ENV = 'development',
+  NEXT_PUBLIC_VERCEL_URL = 'https://localhost:3000',
+  NODE_ENV = 'development',
 } = process.env;
 
 const requestedScope = 'activity:read,activity:write,profile:read_all,read_all';
-// const isProd = NODE_ENV === 'production';
+const isProd = NODE_ENV === 'production';
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -19,9 +19,9 @@ export const authOptions: NextAuthOptions = {
       authorization: {
         params: {
           scope: requestedScope,
-          // redirectUri: isProd
-          //   ? `https://${NEXT_PUBLIC_VERCEL_URL}/api/auth/callback/strava`
-          //   : `${NEXT_PUBLIC_VERCEL_URL}/api/auth/callback/strava`,
+          redirectUri: isProd
+            ? `${NEXT_PUBLIC_VERCEL_URL}/api/auth/callback/strava`
+            : `https://${NEXT_PUBLIC_VERCEL_URL}/api/auth/callback/strava`,
         },
       },
     }),
@@ -36,13 +36,6 @@ export const authOptions: NextAuthOptions = {
         token['refreshToken'] = account.refresh_token;
       }
       return token;
-    },
-    async redirect({ url, baseUrl }) {
-      // Allows relative callback URLs
-      if (url.startsWith('/')) return `${baseUrl}${url}`;
-      // Allows callback URLs on the same origin
-      else if (new URL(url).origin === baseUrl) return url;
-      return baseUrl;
     },
   },
   secret: process.env.NEXT_PUBLIC_AUTH_SECRET,
