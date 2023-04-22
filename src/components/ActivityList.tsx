9896b90a7d0e42ee52fe1d59abb6a07d85a5e934
Raveen Reddy <raveenreddy89@gmail.com>
2023-04-22 12:30:58 -0400

    style: mobile responsive styling for index and activity list

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -8,8 +8,10 @@ type ActivityListProps = {
 
 export default function ActivityList({ activities }: ActivityListProps) {
   return (
-    <div className='flex w-full flex-col space-y-4 border-2 border-slate-200 p-4 shadow-2xl md:max-w-4xl'>
-      <h1 className='mt-8 text-center text-4xl font-light'>Your Activities</h1>
+    <div className='flex w-full flex-col space-y-4 border-slate-200 p-4 md:max-w-4xl'>
+      <h1 className='mt-4 text-center font-light md:text-4xl'>
+        Your Activities
+      </h1>
       {activities.slice(0, 4).map((activity) => (
         <div
           className='border-2 border-slate-500 hover:scale-105 hover:bg-emerald-50'
