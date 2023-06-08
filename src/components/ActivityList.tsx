5d874350efdcbfc50062f7b5a0d0f88fce645732
Raveen Reddy <raveenreddy89@gmail.com>
2023-06-08 15:09:43 -0400

    feat: modify global map list components

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,32 +1,29 @@
-import ActivityRow from '@/components/ActivityRow';
+import MapActivityRow from '@/components/ActivityRow';
 
 import { StravaActivity } from '@/helpers/types';
 
 type ActivityListProps = {
   activities: StravaActivity[];
-  showPolyLineMap: boolean;
-  disableActivityLink: boolean;
+  currentActivityId: string;
+  setCurrentActivity: (activity: StravaActivity) => void;
 };
 
 export default function ActivityList({
   activities,
-  showPolyLineMap,
-  disableActivityLink,
+  setCurrentActivity,
+  currentActivityId,
 }: ActivityListProps) {
   return (
-    <div className='flex flex-col space-y-4 border-slate-200 p-4 shadow-xl md:max-w-4xl'>
-      <h1 className='mt-4 text-center font-light md:text-4xl'>
-        Your Activities
-      </h1>
+    <div className='flex flex-col space-y-8 overflow-auto'>
       {activities.map((activity) => (
         <div
-          className='border-2 border-slate-500 hover:scale-105 hover:bg-emerald-50'
+          className='hover:border-2 hover:border-green-800 hover:bg-green-500 hover:bg-opacity-5'
           key={activity.id}
+          onClick={() => setCurrentActivity(activity)}
         >
-          <ActivityRow
+          <MapActivityRow
             activity={activity}
-            showPolyLineMap={showPolyLineMap}
-            disableActivityLink={disableActivityLink}
+            currentActivityId={currentActivityId}
           />
         </div>
       ))}
