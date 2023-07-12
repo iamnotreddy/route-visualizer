3354d599d9deac7bb6c81c1019d5b58dfbd7a23b
Raveen Reddy <raveenreddy89@gmail.com>
2023-07-11 20:00:50 -0400

    feat: new types, update index flow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,88 @@
 import { useInfiniteQuery } from '@tanstack/react-query';
+import { subDays } from 'date-fns';
+import { useSession } from 'next-auth/react';
+import {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useEffect,
+  useState,
+} from 'react';
 
 import GlobalMap from '@/components/globalMap';
+import { DateRangeInput } from '@/components/hooks/useActivityList';
 
 import { getActivityList } from '@/helpers/fetchingFunctions';
 import { StravaActivity } from '@/helpers/types';
 
+type FetchingContext = {
+  allActivities: StravaActivity[] | undefined;
+  isFetchingNextPage: boolean;
+  isRefetching: boolean;
+  isLoading: boolean;
+  fetchNextPage: () => void;
+  refetch: () => void;
+  dateRange: DateRangeInput;
+  setDateRange: Dispatch<SetStateAction<DateRangeInput>>;
+};
+
+export const FetchingContext = createContext<FetchingContext>(
+  {} as FetchingContext
+);
+
 export default function HomePage() {
+  const { status } = useSession();
+
+  const currentDate = new Date();
+  const priorDate = subDays(currentDate, 30);
+
+  const [dateRange, setDateRange] = useState({
+    startDate: currentDate,
+    endDate: priorDate,
+    isDefault: true,
+  });
+
+  const [allActivities, setAllActivities] = useState<StravaActivity[]>();
+
   const {
     data: activities,
     isFetchingNextPage,
+    isLoading,
     fetchNextPage,
-  } = useInfiniteQuery(
+    refetch,
+    isRefetching,
+  } = useInfiniteQuery<StravaActivity[], Error>(
     ['activities'],
-    ({ pageParam = 1 }) => getActivityList(pageParam),
+    ({ pageParam = 1 }) => getActivityList(pageParam, dateRange),
     {
-      getNextPageParam: (
-        lastPage: StravaActivity[],
-        allPages: Array<StravaActivity[]>
-      ) => {
+      getNextPageParam: (lastPage, allPages) => {
         return lastPage.length ? allPages.length + 1 : undefined;
       },
+      enabled: status === 'authenticated',
     }
   );
 
-  // map all pages into one array
-  const allActivities = activities
-    ? activities.pages.flatMap((page) => page)
-    : [];
+  useEffect(() => {
+    if (status === 'authenticated' && activities) {
+      const newActivities = activities.pages.flatMap((page) => page);
+      setAllActivities(newActivities);
+    }
+  }, [activities, status]);
+
+  const fetchingContextValues = {
+    allActivities,
+    isFetchingNextPage,
+    isRefetching,
+    isLoading,
+    fetchNextPage,
+    refetch,
+    dateRange,
+    setDateRange,
+  };
 
   return (
-    <GlobalMap
-      activities={allActivities}
-      fetchNextPage={fetchNextPage}
-      isFetchingNextPage={isFetchingNextPage}
-    />
-    // )
+    <FetchingContext.Provider value={fetchingContextValues}>
+      <GlobalMap />
+    </FetchingContext.Provider>
   );
 }
