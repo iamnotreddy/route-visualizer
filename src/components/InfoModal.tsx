841df1ea5bdf951ef9df503ea90304c5b9e6507e
Raveen Reddy <raveenreddy89@gmail.com>
2024-01-03 10:02:32 -0500

    feat: infomodal

--- src/components/InfoModal.tsx
+++ src/components/InfoModal.tsx
@@ -9,19 +9,17 @@ export const InfoModal = () => {
       <div className='flex flex-col justify-start space-y-4'>
         <p>
           re.play is a community map of running / biking routes across the
-          world. Its also a visualization tool to enhance your Strava
-          activities. You can relive journeys, explore your accomplishments, and
-          watch captivating route animations—all accompanied by insightful
-          performance charts.
+          world. Its also a visualization tool that adds perspective to your
+          Strava activities.
         </p>
         <p>
-          If you feel compelled, add your favorite routes to the community map
-          to inspire others!
+          If you feel compelled, you can add your favorite routes to the
+          community map to inspire others!
         </p>
 
-        <p className='font-semibold'>
-          Sign in with Strava to visualize your own activities, or browse the
-          map to see where the re.play community has ventured to.
+        <p className='mb-4 text-center font-semibold'>
+          Sign in with Strava to relive your activities by watching captivating
+          route animations accompanied by insightful performance charts.
         </p>
       </div>
 
@@ -40,9 +38,13 @@ export const InfoModal = () => {
           />
         </Button>
       </div>
+      <p className='text-center font-bold'>
+        Or exit this modal to browse the community map to see where re.play
+        users have ventured to.
+      </p>
       <div className='flex max-w-sm flex-row items-center justify-center space-x-2 text-xs font-light'>
         <div className='flex flex-col space-y-1'>
-          <p className='text-center text-xs'>
+          <p className='mt-8 text-center text-xs'>
             re.play reads your activities directly from Strava. Data is only
             saved when you add your activity to the community map. Please be
             mindful about what locations you save to the map.
